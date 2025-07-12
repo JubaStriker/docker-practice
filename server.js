80570db1aad1551a665ba115301183446a998554
JubaStriker <jubastriker@gmail.com>
2025-07-13 03:00:51 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const mongoUrl = process.env.MONGODB_URI
+const dbName = 'users';
+
+// Connect to MongoDB
+MongoClient.connect(mongoUrl, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to MongoDB');
+        db = client.db(dbName);
+
+        // Start the server after DB connection is established
+        app.listen(port, () => {
+            console.log(`Server running at http://localhost:${port}`);
+        });
+    })
+    .catch(error => console.error(error));
+
+// Route 1: Home route
+app.get('/', (req, res) => {
+    res.send('Hello from the Home route!');
+});
+
+// Route 2: About route
+app.get('/about', (req, res) => {
+    res.send('This is the About route.');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
